21ced6e03603aae37f7c9510539498dd2d646079
iccangji <iksanpml131@gmail.com>
2025-02-23 19:45:11 +0800

    database reconnect update

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -2,16 +2,37 @@ const mysql = require('mysql');
 const { logger } = require('../utils/logger');
 const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = require('../utils/secrets');
 
-const connection = mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASS,
-    database: DB_NAME
-});
+let connection;
 
-connection.connect((err) => {
-    if (err) logger.error(err.message);
-    else logger.info('Database connected')
-});
+function handleDisconnect() {
+    connection = mysql.createConnection({
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASS,
+        database: DB_NAME
+    });
+
+    connection.connect((err) => {
+        if (err) {
+            logger.error('Error connecting to database:', err.message);
+            setTimeout(handleDisconnect, 2000); // reconnect database 2 detik
+        } else {
+            logger.info('Database connected');
+        }
+    });
+
+    connection.on('error', (err) => {
+        logger.error('Database error:', err.message);
+
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            logger.warn('Koneksi MySQL terputus, mencoba reconnect...');
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
 
 module.exports = connection;
